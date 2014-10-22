f3629df15ed11aa612a1a35879041ae2410e4c92
geniuszhaoyi <genius.zhaoyi@gmail.com>
2014-10-22 10:58:19 +0800

    Update main.cpp

--- server/a/main.cpp
+++ server/a/main.cpp
@@ -5,7 +5,7 @@
 #include "cJSON/cJSON.h"
 
 #define LEN 20
-#define GENE_LEN 8000000
+#define GENE_LEN 80000000
 #define PAM_LEN 20
 
 int check_pam(const char *str,const char *pam){
@@ -73,78 +73,114 @@ void get_pam_info(char pam_id[],const char pam[]){
 }
 
 int sum=0;
-void add(FILE *fout,char *str){
+char sql[500000]="";
+void add(char *str){
     if(sum==0){
-        printf(".");
-        fflush(fout);
-        fprintf(fout,"INSERT INTO Table_sgRNA (Chr_No, sgrna_start, sgrna_end, sgrna_strand, sgrna_seq, sgrna_PAM, pam_id) VALUES \n");
+        strcat(sql,"INSERT INTO Table_sgRNA (Chr_No, sgrna_start, sgrna_end, sgrna_strand, sgrna_seq, sgrna_PAM, pam_id) VALUES \n");
     }else{
-        fprintf(fout,",\n");
+        strcat(sql,",\n");
     }
-    fprintf(fout,"%s",str);
+    strcat(sql,str);
     sum=(sum+1)%500;
     if(sum==0){
-        fprintf(fout,"; \n");
+        strcat(sql,"; \n");
+        int res=mysql_query(my_conn,sql);
+        if(res){
+            printf("%s\n",mysql_error(my_conn));
+        }
+        sql[0]=0;
     }
 }
-void finish(FILE *fout){
+void finish(){
     if(sum!=0){
-        fprintf(fout,"; \n");
+        strcat(sql,"; \n");
+        int res=mysql_query(my_conn,sql);
+        if(res){
+            printf("%s\n",mysql_error(my_conn));
+            printf("%s\n",sql);
+        }
+        sql[0]=0;
     }
 }
 
+int getFileID(const char *user_ID,const char *fileName){
+    char sql[1024];
+    sprintf(sql,"SELECT upload_ID FROM user_upload WHERE user_id=%s AND upload_filename='%s'; ",user_ID,fileName);
+    int res=mysql_query(my_conn,sql);
+    if(res){
+        return -1;
+    }
+    result=mysql_store_result(my_conn);
+    if(result){
+        sql_row=mysql_fetch_row(result);
+        return atoi(sql_row[0]);
+    }else{
+        return -1;
+    }
+    return -1;
+}
+
+//char def1[]="{\"specie\":\"kkkk\",\"files\":[{\"fileName\":\"NC_01297\"}],\"PAM\":\"NGG\"}";
+//char def2[]="8"
 int main(int args, char *argv[]){
-    if(args!=2){
-        printf("N");
+    if(args!=3){
+        printf("N3");
         return 1;
     }
 
     int count=0;
 
+    my_bool mb=false;
     my_conn=mysql_init(NULL);
-    if(mysql_real_connect(my_conn,"127.0.0.1","root","root","CasDB",3306,NULL,0)){
+    mysql_options(my_conn,MYSQL_SECURE_AUTH,&mb);
+#ifdef  _WIN32
+    if(mysql_real_connect(my_conn,"127.0.0.1","root","zy19930108","CasDB",3306,NULL,0)){
+#endif
+#ifdef  __linux
+    if(mysql_real_connect(my_conn,"127.0.0.1","igem","uestc2014!","CasDB",3306,NULL,0)){
+#endif
         cJSON *root=cJSON_Parse(argv[1]);
         if(root==NULL){
-            printf("N");
+            printf("N4");
             return 1;
         }
-        
-        char Sno[15];
-        mysql_query(my_conn,"LOCK TABLES Table_Specie WRITE");
-        mysql_query(my_conn,"SELECT max(Sno) FROM Table_Specie; ");
-        result=mysql_store_result(my_conn);
-        sql_row=mysql_fetch_row(result);
-        sprintf(buffer,"INSERT INTO Table_Specie(SName) VALUES('%s0'); ",sql_row[0]);
-        printf("%s0",sql_row[0]);
+
+        char Sno[15],SName[100+1];
+        strcpy(SName,cJSON_GetObjectItem(root,"specie")->valuestring);
+		if(strcmp(argv[2],"0")==0) sprintf(buffer,"INSERT INTO Table_Specie(SName) VALUES('%s-%s'); ",argv[2],SName);
+        else sprintf(buffer,"INSERT INTO Table_Specie(SName,user_id) VALUES('%s-%s',%s); ",argv[2],SName,argv[2]);
+        printf("%s-%s",argv[2],SName);
         mysql_query(my_conn,buffer);
+        result=mysql_store_result(my_conn);
         mysql_free_result(result);
         mysql_query(my_conn,"SELECT LAST_INSERT_ID(); ");
         result=mysql_store_result(my_conn);
         sql_row=mysql_fetch_row(result);
         strcpy(Sno,sql_row[0]);
         mysql_free_result(result);
-        mysql_query(my_conn,"UNLOCK TABLES");
-        
+printf("\n%s\n",Sno);
         char pam[20+1],pam_id[15];
         strcpy(pam,cJSON_GetObjectItem(root,"PAM")->valuestring);
         get_pam_info(pam_id,pam);
         int req_pam_len=strlen(pam);
-        
+
         cJSON *files=cJSON_GetObjectItem(root,"files");
-        FILE *fout=fopen("tmp/out.sql","w");
         int len=cJSON_GetArraySize(files);
+
         for(int k=0;k<len;k++){
             char Chr_No[15];
             cJSON *node=cJSON_GetArrayItem(files,k);
-            sprintf(buffer,"INSERT INTO Table_chromosome(SNo,Chr_Name) VALUES('%s','%s'); ",Sno,cJSON_GetObjectItem(node,"chromosomeName")->valuestring);
+            sprintf(buffer,"INSERT INTO Table_chromosome(SNo,Chr_Name) VALUES('%s','%s'); ",Sno,cJSON_GetObjectItem(node,"fileName")->valuestring);
             mysql_query(my_conn,buffer);
             mysql_query(my_conn,"SELECT LAST_INSERT_ID(); ");
             result=mysql_store_result(my_conn);
             sql_row=mysql_fetch_row(result);
             strcpy(Chr_No,sql_row[0]);
             mysql_free_result(result);
-            
-                sprintf(buffer,"uploaddata/%s",cJSON_GetObjectItem(node,"file_ID")->valuestring);
+                
+                if(strcmp(argv[2],"0")==0 && cJSON_GetObjectItem(node,"filePath")) sprintf(buffer,cJSON_GetObjectItem(node,"filePath")-valuestring);
+                else sprintf(buffer,"../uploaddata/%d",getFileID(argv[2],cJSON_GetObjectItem(node,"fileName")->valuestring));
+printf("%s\n",buffer);
                 FILE *ff=fopen(buffer,"r");
                 readLine(ff);
                 int i=1,j;
@@ -158,15 +194,15 @@ int main(int args, char *argv[]){
                 len=i;
                 fclose(ff);
 
-                char thispam[20],nt[LEN+1];           //IMPORTANT!!!      Here are some problems, fixed in Huanglaoshi's Server
+                char thispam[20],nt[LEN+1];
                 for(i=LEN;i<len-req_pam_len;i++){       // All possible gRNAs, +direction
                     if(check_pam(str+i,pam)){
                         for(j=0;j<LEN;j++) nt[j]=(str+i-LEN)[j];
                         nt[j]=0;
                         for(j=0;j<req_pam_len;j++) thispam[j]=(str+i)[j];
                         thispam[j]=0;   //(Chr_No, sgrna_start, sgrna_end, sgrna_strand, sgrna_seq, sgrna_PAM, pam_id)
-                        sprintf(buffer,"(%d, %d, %d, '%c', '%s', '%s', %s)",Chr_No,i-LEN,i-1+req_pam_len,'+',nt,thispam,pam_id);
-                        add(fout,buffer);
+                        sprintf(buffer,"(%s, %d, %d, '%c', '%s', '%s', %s)",Chr_No,i-LEN,i-1+req_pam_len,'+',nt,thispam,pam_id);
+                        add(buffer);
                         count++;
                     }
                 }
@@ -178,17 +214,14 @@ int main(int args, char *argv[]){
                         nt[j]=0;
                         for(j=0;j<req_pam_len;j++) thispam[j]=dna_rev_char((str+i)[req_pam_len-j-1]);
                         thispam[j]=0;
-                        sprintf(buffer,"(%d, %d, %d, '%c', '%s', '%s', %s)",Chr_No,i,i+req_pam_len+LEN-1,'-',nt,thispam,pam_id);
-                        add(fout,buffer);
+                        sprintf(buffer,"(%s, %d, %d, '%c', '%s', '%s', %s)",Chr_No,i,i+req_pam_len+LEN-1,'-',nt,thispam,pam_id);
+                        add(buffer);
                         count++;
                     }
                 }
-
         }
-        finish(fout);
-        fclose(fout);
+        finish();
     }
-    mysql_free_result(result);
     mysql_close(my_conn);
 
     return 0;
